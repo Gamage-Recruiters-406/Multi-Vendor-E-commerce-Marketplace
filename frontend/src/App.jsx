f3140e6d35ce3db249ce0444d64a804c5e59f078
Keshan Bhanuka Wijesinghe <it22206800@my.sliit.lk>
2026-04-20 11:17:54 +0530

    feat: announcements

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,16 @@
-import { 
-  BrowserRouter as Router, 
-  Routes, 
-  Route 
-} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import SigninPage from './pages/login/SigninPage';
-import SignupPage from './pages/login/SignupPage';
-import VendorProductManagemnt from './pages/Vendor/ProductManagement';
-import Products from './pages/Vendor/Products';
-import CreateStore from './pages/Vendor/CreateStore';
-import ViewStore from './pages/Vendor/ViewStore';
+import SigninPage from "./pages/login/SigninPage";
+import SignupPage from "./pages/login/SignupPage";
+import VendorProductManagemnt from "./pages/Vendor/ProductManagement";
+import Products from "./pages/Vendor/Products";
+import CreateStore from "./pages/Vendor/CreateStore";
+import ViewStore from "./pages/Vendor/ViewStore";
+import Announcements from "./pages/Announcements";
+import CreateAnnouncement from "./pages/CreateAnnouncement";
+import EditAnnouncement from "./pages/EditAnnouncement";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
@@ -25,10 +24,24 @@ function App() {
         <Route path="/signup" element={<SignupPage />} />
 
         {/* Vendor Module (REQ4 & REQ5) */}
-        <Route path="/vendor/product_management" element={<VendorProductManagemnt />} />
+        <Route
+          path="/vendor/product_management"
+          element={<VendorProductManagemnt />}
+        />
         <Route path="/vendor/products" element={<Products />} />
         <Route path="/vendor/create-store" element={<CreateStore />} />
         <Route path="/vendor/store/:id" element={<ViewStore />} />
+
+        {/* Admin Module */}
+        <Route path="/admin/announcements" element={<Announcements />} />
+        <Route
+          path="/admin/announcements/create"
+          element={<CreateAnnouncement />}
+        />
+        <Route
+          path="/admin/announcements/edit/:id"
+          element={<EditAnnouncement />}
+        />
       </Routes>
     </Router>
   );
